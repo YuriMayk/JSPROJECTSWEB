d6f437a6914f19252177b0d06fa3a39f4a3579e7
Yuri Mayk <yuri.mayk@gmail.com>
2022-01-30 14:49:05 -0300

    Created and Developed Projeto 02 folder.

---
+++ Projeto 02/app.js
@@ -0,0 +1,28 @@
+//Set initial count
+let count = 0;
+// select value and buttons
+let value = document.querySelector("#value")
+const btns = document.querySelectorAll(".btn")
+
+btns.forEach(function(btn) {
+    btn.addEventListener("click", function(button){
+        const styles = button.currentTarget.classList;
+        if ( styles.contains("decrease")){
+            count--;
+        } else if (styles.contains("increase")){
+            count++;
+        } else {
+            count = 0;
+        }
+        value.textContent = count;
+        if(count>0) {
+            value.style.color = "green";
+        }
+        if(count<0) {
+            value.style.color = "red";
+        }
+        if(count === 0) {
+            value.style.color = "#222";
+        }
+    })
+})
